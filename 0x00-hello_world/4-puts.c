a5e8cab568fd8c996a0179e995760331d2e070e2
Lewis Murgor <kiplagatlewis29@gmail.com>
2023-02-18 03:14:59 -0800

    add comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * Main - Entry point
+ * Uses puts function to write a string
+ * /
+ 
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
