302820312fd02dab8bec0d2e6fd8b99c4fdf76e0
Lewis Murgor <kiplagatlewis29@gmail.com>
2023-02-18 03:34:58 -0800

    create a c program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * Uses puts function to write a string
+ * main - Entry point
+ * Return:0(success)
  */
  
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
