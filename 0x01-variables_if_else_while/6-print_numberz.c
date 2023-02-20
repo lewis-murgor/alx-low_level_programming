5c1defaad36b19ce0a67818d632e1820c1fa594b
Lewis Murgor <kiplagatlewis29@gmail.com>
2023-02-20 07:50:28 -0800

    create a C programme that prints all single digit numbers starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int numbers;
+for (numbers = '0'; numbers <= '9'; numbers++)
+{
+putchar(numbers);
+}
+putchar('\n');
+return (0);
+}
