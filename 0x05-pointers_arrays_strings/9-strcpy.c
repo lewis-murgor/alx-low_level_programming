4b0a970f560cac6becc8e1216452d6ea9ee43934
Lewis Murgor <kiplagatlewis29@gmail.com>
2023-02-27 12:21:08 -0800

    create a function that copies a string to another pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string pointed to by src to the buffer pointed by dest
+ * @dest: char pointer
+ * @src: char pointer 2
+ * Return: dest pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, count;
+
+	len = 0;
+
+	while (src[len] != '\0')
+		len++;
+	for (count = 0; count <= len; count++)
+		dest[count] = src[count];
+	return (dest);
+}
